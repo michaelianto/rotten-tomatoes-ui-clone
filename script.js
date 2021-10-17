7cf065ca87fcbee42d2076942ffbccdc11c83f8d
michaelianto <61343284+michaelianto@users.noreply.github.com>
2021-10-17 23:18:28 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+function functionOne(){
+    $(".header").css("background", "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(\"Assets/Header_Poster_1.jpg\")")
+    $(".header").css("background-repeat", "no-repeat")
+    $(".header").css("background-size", "100%")
+    $(".movie-title").text("Deadpool 2")
+    $(".movie-year").text("(2018)")
+    $(".tomatometer-score").text("83%")
+    $(".audience-score").text("85%")
+    $(".meter span").css("width", "84%")
+    $(".red span").css("background-image", "linear-gradient(#f0a3a3, #f42323)")
+}
+
+function functionTwo(){
+    $(".header").css("background", "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(\"Assets/Header_Poster_2.jpg\")")
+    $(".header").css("background-repeat", "no-repeat")
+    $(".header").css("background-size", "100%")
+    $(".movie-title").text("Solo:")
+    $(".movie-year").text("A Star Wars Story(2018)")
+    $(".tomatometer-score").text("70%")
+    $(".audience-score").text("64%")
+    $(".meter span").css("width", "67%")
+    $(".red span").css("background-image", "linear-gradient(#f0a3a3, #f4a023)")
+//     $('#tip').css({'display','none'});
+// },800);
+    
+}
+
+function functionThree(){
+    $(".header").css("background", "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(\"Assets/Header_Poster_3.jpg\")")
+    $(".header").css("background-repeat", "no-repeat")
+    $(".header").css("background-size", "100%")
+    $(".movie-title").text("A Star is Born")
+    $(".movie-year").text("(2018)")
+    $(".tomatometer-score").text("90%")
+    $(".audience-score").text("81%")
+    $(".meter span").css("width", "85%")
+    $(".red span").css("background-image", "linear-gradient(#f0a3a3, #f1f423)")
+}
+
+function functionFour(){
+    $(".header").css("background", "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(\"Assets/Header_Poster_4.jpg\")")
+    $(".header").css("background-repeat", "no-repeat")
+    $(".header").css("background-size", "100%")
+    $(".movie-title").text("Aquaman")
+    $(".movie-year").text("(2018)")
+    $(".tomatometer-score").text("64%")
+    $(".audience-score").text("79%")
+    $(".meter span").css("width", "71%")
+    $(".red span").css("background-image", "linear-gradient(#f0a3a3, #23b9f4)")
+}
+
+function functionFive(){
+    $(".header").css("background", "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(\"Assets/Header_Poster_5.jpg\")")
+    $(".header").css("background-repeat", "no-repeat")
+    $(".header").css("background-size", "100%")
+    $(".movie-title").text("Spiderman:")
+    $(".movie-year").text("Into The Spider-Verse (2018)")
+    $(".tomatometer-score").text("97%")
+    $(".audience-score").text("94%")
+    $(".meter span").css("width", "95%")
+    $(".red span").css("background-image", "linear-gradient(#f0a3a3, #f42323)")
+}
+
+
+// background-repeat: no-repeat;
+//     background-size: 100%;
+//     transition: opacity 2s ease-in;
